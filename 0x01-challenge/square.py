931ac753b043c4854449743aa6b4314e77979593
Manuel Figueroa <manuelthemuffin@gmail.com>
2021-05-24 03:19:39 +0000

    added task 1 again

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
+""" square object """
+
 
 class square():
 
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """ sets all square object values """
         for key, value in kwargs.items():
             setattr(self, key, value)
-        
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Returns the perimeter """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
